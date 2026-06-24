cfd25ad478b4f59ff0be869a28cbec38e496cd53
Efe <efe_okoh@yahoo.com>
2026-06-25 00:46:48 +1000

    commit cd1a2b3e4f5678901234567890abcdef12345678

--- run_sistr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PREVIEW="${1:-}"
-
-nextflow run sistr_pipeline.nf \
-    --input_s3 "s3://your-bucket-name/validationset" \
-    --outdir "s3://your-bucket-name/sistr_results" \
-    $PREVIEW
